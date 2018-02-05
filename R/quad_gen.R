ef9198ff77dd7c4e7e5fdec0a1a5204d5dcc25bc
pbotter42 <pdbotter@gmail.com>
2018-02-04 20:39:42 -0700

    removed warnings

--- R/quad_gen.R
+++ R/quad_gen.R
@@ -10,22 +10,13 @@
 #'
 #' @return q_points
 #'
-#' @examples quad_gen(n_quad=21,theta_min=-5, theta_max=5)
+#' @examples quad_gen(n_quad=21, theta_min=-5, theta_max=5)
 #'
 #' @export quad_gen
 
 
 quad_gen <- function(n_quad, theta_min, theta_max) {
-  if(theta_max <= 0 |
-     theta_min >= 0 |
-     theta_max/theta_min != -1) {
-    stop("Improper theta_min and or theta_max values are
-         provided. Ensure values have the proper sign and
-         are proportional.")
-  }
-  if((n_quad %% 2) != 1) {
-    warning("An odd number of quadriture points was not provided.")
-  }
+
   q_points <- seq(from = theta_min, to = theta_max,
                   by = (theta_max-theta_min)/(n_quad-1))
   return(q_points)
