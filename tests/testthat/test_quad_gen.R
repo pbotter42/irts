50a06da82153d4527daf06bfc456797f63f0834a
pbotter42 <pdbotter@gmail.com>
2018-02-17 09:22:23 -0700

    test now reflects the removal of quad_gen warning

--- tests/testthat/test_quad_gen.R
+++ tests/testthat/test_quad_gen.R
@@ -8,10 +8,3 @@ test_that("quad_gen produces 5 quad points", {
   expect_equal(theta, c(-2, -1, 0, 1, 2)) 
 }) 
 
-test_that("quad_gen produces correct errors", { 
-  expect_warning(quad_gen(n_quad = 112, 
-                          theta_min = -2, 
-                          theta_max = 2), 
-                 "An odd number of quadrature points was not provided.") 
-  
-}) 
