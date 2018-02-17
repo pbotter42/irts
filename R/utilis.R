e95e03a475d8ba9c1dce65878f845ad10da92dcf
pbotter42 <pdbotter@gmail.com>
2018-02-17 08:56:05 -0700

    fixed dup

--- R/utilis.R
+++ R/utilis.R
@@ -104,16 +104,3 @@ comp_ts <- function(theta_gen,
   }   
   return(ts_ic)   
 }
-
-norm_dist_2d <- function(theta_gen, theta_spec) {
-  dist_m <- matrix(0,length(theta_gen),length(theta_gen))
-  phi <- 0#toDo
-  for (i in 1:length(theta_gen)) {
-    for (j in 1:length(theta_spec)) {
-      dist_m[i,j] <- exp(-0.5*(theta_gen[i]^2+theta_spec[j]^2-2*phi*theta_gen[i]*theta_spec[j])/(1-phi*phi))
-    }
-  }
-  dist_m <- dist_m/sum(dist_m)
-  return(dist_m)
-  
-}
